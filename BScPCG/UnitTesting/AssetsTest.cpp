b4cb64862c5ef0715f4bad23823b982ae0acbb70
Jaddori <ohlsson.niclas@gmail.com>
2017-04-05 23:46:41 +0200

    Updated tests to reflect changes in structure.

--- BScPCG/UnitTesting/AssetsTest.cpp
+++ BScPCG/UnitTesting/AssetsTest.cpp
@@ -9,26 +9,26 @@ TEST(AssetsTest, LoadValidModel)
 {
 	AssetManager assets;
 
-	EXPECT_GE(assets.Load<Model>("./assets/models/valid_model.model" ), 0);
+	EXPECT_GE(assets.LoadModel("./assets/models/valid_model.model" ), 0);
 }
 
 TEST(AssetsTest, LoadInvalidModel)
 {
 	AssetManager assets;
 
-	EXPECT_EQ(assets.Load<Model>("./assets/models/invalid_model.model"), -1);
+	EXPECT_EQ(assets.LoadModel("./assets/models/invalid_model.model"), -1);
 }
 
 TEST(AssetsTest, LoadValidTexture)
 {
 	AssetManager assets;
 
-	EXPECT_GE(assets.Load<Texture>("./assets/textures/valid_texture.dds"), 0);
+	EXPECT_GE(assets.LoadTexture("./assets/textures/valid_texture.dds"), 0);
 }
 
 TEST(AssetsTest, LoadInvalidTexture)
 {
 	AssetManager assets;
 
-	EXPECT_EQ(assets.Load<Texture>("./assets/textures/invalid_texture.dds"), -1);
+	EXPECT_EQ(assets.LoadTexture("./assets/textures/invalid_texture.dds"), -1);
 }
--- BScPCG/UnitTesting/RenderQueueTest.cpp
+++
@@ -1,4 +0,0 @@
-#include "gtest\gtest.h"
-#include "RenderQueue.h"
-
-using namespace Rendering;
