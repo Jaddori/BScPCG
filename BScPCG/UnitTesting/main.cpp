62141210b77326b75ab05c7c1744c1be2ca80690
Elias <elias.frank.@gmail.com>
2017-05-04 14:10:37 +0200

    PCG - main unfucked

--- BScPCG/UnitTesting/main.cpp
+++ BScPCG/UnitTesting/main.cpp
@@ -1,31 +1,8 @@
 #include "gtest\gtest.h"
-#include "DataManager.h"
-#include "District.h"
-#include "Building.h"
-#include "PerlinNoise.h"
-
-using namespace PCG;
 
 int main( int argc, char** argv )
 {
 	::testing::InitGoogleTest( &argc, argv );
-
-	DataManager* dataManager = new DataManager();
-	District* district = new District();
-	Building* building = new Building();
-
-	dataManager->addDataHolder(district);
-	dataManager->addDataHolder(building);
-
-	dataManager->collectData();
-	dataManager->collectData();
-	dataManager->collectData();
-	dataManager->showData();
-	float t1 = dataManager->getData("building");
-	int derp;
-
-	PerlinNoise perlin(34534575);
-	
-	std::cin >> derp;
-	return RUN_ALL_TESTS( );
+	int testResults = RUN_ALL_TESTS();
+	return testResults;
 }
